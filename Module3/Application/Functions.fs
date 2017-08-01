68166fd77e102aa97ab87fcb7f9889d5f9cec98e
Pablo Alarcon <pablo.alarcon@outlook.com>
2017-08-01 17:02:53 +0200

    Upgraded to VS 2017. Implemented Module 3

--- Module3/Application/Functions.fs
+++ Module3/Application/Functions.fs
@@ -13,9 +13,32 @@ let getPurchases customer =
     else (customer, 80M)
 
 let increaseCredit condition customer =
-    if condition customer then { customer with Credit = customer.Credit + 100M }
-    else { customer with Credit = customer.Credit + 50M }
+    if condition customer then { customer with Credit = customer.Credit + 100M<USD> }
+    else { customer with Credit = customer.Credit + 50M<USD> }
 
 let increaseCreditUsingVip = increaseCredit (fun c -> c.IsVip)
 
 let upgradeCustomer = getPurchases >> tryPromoteToVip >> increaseCreditUsingVip
+
+
+//Create a function called “isAdult” in Module3/Application/Functions.fs that
+// Receives a customer as parameter
+// Returns false if the PersonalDetails are not defined (None)
+// Returns true if the customer is 18 years of age or older, or false otherwise
+
+let isAdult customer = 
+    match customer.PersonalDetails with
+    | Some details -> details.DateOfBirth.AddYears 18 <= DateTime.Now.Date
+    | None -> false
+
+//    4.1. Create a function called “getAlert” in Module3/Application/Functions.fs that
+// Receives a customer as parameter
+// Returns “Alert for customer [Id]” if the customer allowed alerts or returns an empty string otherwise.
+
+
+let getAlert customer = 
+    match customer.Notifications with
+    //REMARKS : Uhm, with named tuples parameters we use ';' separators but ',' for unnamed!?
+    //| ReceiveNotifications (receiveDeals = _; receiveAlerts = true) -> sprintf "Alert for customer %i " customer.Id
+    | ReceiveNotifications ( _, true) -> sprintf "Alert for customer %i" customer.Id
+    | _ -> String.Empty
--- Module3/Application/Types.fs
+++ Module3/Application/Types.fs
@@ -2,8 +2,43 @@
 
 open System
 
+
+type [<Measure>] EUR
+type [<Measure>] USD
+
+type PersonalDetails = {
+    FirstName : string
+    LastName : string
+    DateOfBirth : DateTime
+}
+
+type Notifications =
+    | NoNotifications
+    | ReceiveNotifications of receiveDeals:bool * receiveAlerts:bool
+
 type Customer = {
     Id: int
     IsVip: bool
-    Credit: decimal
+    Credit: decimal<USD>
+    PersonalDetails: PersonalDetails option
+    Notifications: Notifications
 }
+
+//1.1. Go to the Module3/Application, open Types.fs and create the following types (above the existing Customer
+//type):
+// A record called “PersonalDetails” with the following fields:
+//o FirstName: string
+//o LastName: string
+//o DateOfBirth: DateTime
+// Two units of measure: “EUR” and “USD”.
+// A discriminated union called “Notifications” with the following cases:
+//o NoNotifications
+//o ReceiveNotification of receiveDeals: bool * receiveAlerts: bool
+//Then add the following new fields to the Customer:
+// PersonalDetails: PersonalDetails option
+// Notifications: Notifications
+//Finally update the Credit field to use the decimal<USD> type
+
+
+
+
